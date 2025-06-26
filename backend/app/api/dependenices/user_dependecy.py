2d6d0d10c2cf47eeb36f44cc8e9ca407b688f115
Ruslan Gatin <Kolpak603@@gmail.com>
2025-06-26 04:12:23 +0300

    qsave

--- backend/app/api/dependenices/user_dependecy.py
+++ backend/app/api/dependenices/user_dependecy.py
@@ -1,9 +1,12 @@
 from app.infrastructure.db.session import fastapi_get_db 
 from typing import Annotated
 
-from fastapi import Depends, Header
+from fastapi import Depends, Header, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.infrastructure.repositories.UserRepository import UserRepository
+
+from logging import getLogger
+logger = getLogger(__name__)
 
 db_dep = Annotated[AsyncSession, Depends(fastapi_get_db)]
 
@@ -12,9 +15,10 @@ async def get_current_user(
 ):
     if authorization is None:
         return None
-    user = await UserRepository.get_user_by_tg_id(authorization)
-    
-    if user is None:
-        raise ValueError("User not found")
 
+    user = await UserRepository.get_user_by_tg_id(authorization)
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
     return user
--- backend/app/models/dtoModels/UserDTO.py
+++ backend/app/models/dtoModels/UserDTO.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 
 class UserDTO(Entity):
-    id: Optional[int]
+    id: Optional[int] = None
     tg_id: int
     name: str
     photo_url: str
