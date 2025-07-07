37082f5545e8460cfcd138cdf1f744acb45cea88
Ruslan Gatin <Kolpak603@@gmail.com>
2025-07-07 04:32:22 +0300

    fix: /user_role/{company_id}/{user_id}

--- backend/app/infrastructure/repositories/UserCompanyRepository.py
+++ backend/app/infrastructure/repositories/UserCompanyRepository.py
@@ -113,9 +113,9 @@ class UserCompanyRepository(IUserCompanyRepository):
         query = select(UserCompany).where(UserCompany.company_id == company_id and UserCompany.user_id == user_id)
         result = await self.session.execute(query)
         usercompany = result.scalars().all()
-        print(123123, usercompany)
-        usercompany_dto = await self.__to_dto(usercompany) if usercompany else None
         
+        usercompany_dto = await self.__to_dto(usercompany) if usercompany else None
+        print(123123, usercompany_dto)
         return usercompany_dto
 
     async def __to_dto(self, usercompany: UserCompany) -> UserCompanyDTO:
