c3e95a8d561afcccb3a6a6e0f2b53147e2fed10a
Ruslan Gatin <Kolpak603@@gmail.com>
2025-07-07 05:03:40 +0300

    fix: put user_company

--- backend/app/api/routes/CompanyRoute.py
+++ backend/app/api/routes/CompanyRoute.py
@@ -45,6 +45,7 @@ async def get_confirmation_info(
 async def update_user_company_role(
     uc_id: int, new_user_company: UserCompanyDTO, session: AsyncSession = Depends(fastapi_get_db)
 ) -> UserCompanyDTO:
+    print(666, uc_id, new_user_company)
     service = CompanyService(session)
     updated_uc = await service.update_user_company(uc_id, new_user_company)
     return updated_uc
