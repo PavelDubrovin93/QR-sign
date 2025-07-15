d6192f8fd4ee1ad5009a32f0177f88a6ed0beb3a
Ruslan Gatin <Kolpak603@@gmail.com>
2025-07-15 22:48:31 +0300

    shit: no understanding

--- src/api/user/get-user-role.ts
+++ src/api/user/get-user-role.ts
@@ -9,7 +9,7 @@ export interface UserRoleResponse {
 
 export const getUserRole = async (companyId: number): Promise<UserRoleResponse> => {
   const webapp = window.Telegram?.WebApp;
-  const telegramUserId = webapp?.initDataUnsafe?.user?.id || 77777777777; //webapp?.initDataUnsafe?.user?.id || 77777777777;
+  const telegramUserId = webapp?.initDataUnsafe?.user?.id || 601732567; //webapp?.initDataUnsafe?.user?.id || 601732567;
 
   const response = await fetch(
     `${config.BACKEND_URL}/${apiPrefix.api}/user_data/user_role/${companyId}`,
