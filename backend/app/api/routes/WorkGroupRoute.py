7318a0b216bd65eed4dfc7b4f958a3839d7f6654
Ruslan Gatin <Kolpak603@@gmail.com>
2025-07-18 00:57:39 +0300

    fix: rollback operations

--- backend/app/api/routes/WorkGroupRoute.py
+++ backend/app/api/routes/WorkGroupRoute.py
@@ -3,6 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+
 from app.api.dependenices.user_dependecy import get_current_user
 from app.infrastructure.db.session import fastapi_get_db
 from app.services.TaskBoardService import TaskBoardService
