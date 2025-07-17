a367a454808cb978ceb418917fe518eb11150573
chereponalex <lexa_klav@mail.ru>
2025-07-17 17:24:44 +0300

    fix user tg id

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ function App() {
     webapp.setBackgroundColor(webapp.themeParams.secondary_bg_color);
   }
 
-  const token_mock = /* webapp?.initDataUnsafe?.user?.id || */ 601732567; //webapp?.initDataUnsafe?.user?.id || 601732567;
+  const token_mock = webapp?.initDataUnsafe?.user?.id || 601732567; //webapp?.initDataUnsafe?.user?.id || 601732567;
 
   const fetchUserRoleByUserId = async (userId: number | null, defaultCompanyId: number | null) => {
     if (!userId) {
